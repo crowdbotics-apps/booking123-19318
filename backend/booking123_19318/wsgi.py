eaec3c604fa36ebcf31b7c467fad6f823bf39ceb
Crowdbotics <team@crowdbotics.com>
2020-08-02 07:32:11 +0000

    Update models

--- backend/booking123_19318/wsgi.py
+++ backend/booking123_19318/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking123_19318.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking123_19318.settings")
 
 application = get_wsgi_application()
